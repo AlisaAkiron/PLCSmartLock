c30b8b3dad1076c1c653bb7fba579c5dee145c30
Liam Sho <conan_creeper@outlook.com>
2022-03-20 15:26:10 +0800

    init.添加 PLCHost 上位机应用

---
+++ src/PLCHost/Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMasaBlazor();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
